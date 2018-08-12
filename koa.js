63d217488509ec399b36d20ca1810c2abbaab93b
yorts52 <yorts52@qq.com>
2018-08-12 17:25:48 +0800

    feat: update case

--- koa.js
+++ koa.js
@@ -2,17 +2,21 @@
 const Koa = require('koa');
 const app = new Koa();
 
-// app.use(async (ctx, next) => {
-//   console.log(1);
-//   await next();
-//   console.log(2);
-// });
+app.use(async (ctx, next) => {
+  console.log(1); // 1
+  await next(); // 2
+  console.log(2); // 8
+});
 
-// app.use(async (ctx, next) => {
-//   console.log(3);
-//   await next();
-//   console.log(4);
-// });
+app.use(async (ctx, next) => {
+  console.log(3); // 3
+  await new Promise((resolve => {
+    setTimeout(resolve, 300);
+  })); // 4
+  
+  await next(); // 5
+  console.log(4); // 7
+});
 
 // app.use(require('./koa-error'))
 
@@ -25,7 +29,9 @@ const app = new Koa();
 // });
 
 app.use(async (ctx, next) => {
+  // 6
   ctx.body = 'Hello World';
+  // throw new Error('hehe')
 });
 
 
